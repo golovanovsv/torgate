e9f1aebbf07c868f9dc4c2868a05de4662efc7b4
golovanovsv <golovanovsv@gmail.com>
2018-11-30 00:29:57 +0500

    Add ContactInfo and fingerprint option

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,48 +1,59 @@
 #!/bin/bash
 
-TCONF="/etc/torrc"
+# Tor manual - https://www.torproject.org/docs/tor-manual.html.en
 
-if [ ! -s $TCONF ]; then
+TCONF="/etc/torrc"
+FINGER_FILE="/var/lib/tor/fingerprint"
+
+if [[ ! -s "$TCONF" ]]; then
 
     # By default TOR starting as SOCKS proxy
     echo "Making TOR configuration file"
     echo "Log notice stdout" > $TCONF
 
-    if [ -n "$HEARTBEATPERIOD" ]; then
+    if [[ -n "$HEARTBEATPERIOD" ]]; then
         echo "HeartbeatPeriod $HEARTBEATPERIOD minutes" >> $TCONF
     else
         echo "HeartbeatPeriod 30 minutes" >> $TCONF
     fi
 
-    if [ -n "$SOCKSPORT" ]; then
+    if [[ -n "$SOCKSPORT" ]]; then
         echo "SocksPort 0.0.0.0:$SOCKSPORT" >> $TCONF
     else
         echo "SocksPort 0.0.0.0:9150" >> $TCONF
     fi
 
     # Enable relay mode
-    if [ -n "$RELAYPORT" ]; then
+    if [[ -n "$RELAYPORT" ]]; then
         echo "ORPort 0.0.0.0:$RELAYPORT" >> $TCONF
     fi
 
     # Enable exit relay
-    if [ -n "$EXITNODE" ]; then
+    if [[ -n "$EXITNODE" ]]; then
         echo "ExitRelay 1" >> $TCONF
     else
         echo "ExitRelay 0" >> $TCONF
     fi
 
-    if [ -n "$NICKNAME" ]; then
+    if [[ -n "$NICKNAME" ]]; then
         echo "Nickname $NICKNAME" >> $TCONF
     fi
 
-    if [ -n "$CONTACTINFO" ]; then
+    if [[ -n "$CONTACTINFO" ]]; then
         echo "ContactInfo $CONTACTINFO" >> $TCONF
     fi
 
-    if [ -n "$CONTROLPORT" ] && [ -n "$HASHEDCONTROLPASSWORD" ]; then
+    if [[ -n "$CONTROLPORT" ]] && [[ -n "$HASHEDCONTROLPASSWORD" ]]; then
         echo "ControlPort $CONTROLPORT" >> $TCONF
         echo "HashedControlPassword $HASHEDCONTROLPASSWORD" >> $TCONF
+    fi
+
+    if [[ -n "$CONTACTINFO" ]]; then
+        echo "ContactInfo $CONTACTINFO" >> $TCONF
+    fi
+
+    if [[ -n "$FINGERPRINT" ]]; then
+        echo "$FINGERPRINT" > $FINGER_FILE
     fi
 fi
 
